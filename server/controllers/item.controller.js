07b900fe78a0959248009a12a435e3c3df5672d7
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-07-08 00:00:08 +0200

    format item controller with exports

--- server/controllers/item.controller.js
+++ server/controllers/item.controller.js
@@ -2,10 +2,10 @@ const Item = require('../models/Item');
 
 /**
  * create a new item
- * @param {Express.Request} req 
- * @param {Express.Response} res 
+ * @param {Express.Request} req
+ * @param {Express.Response} res
  */
-function createItem(req, res) {
+exports.createItem = (req, res) => {
     delete req.body._id;
     const item = new Item({
         ...req.body,
@@ -13,50 +13,48 @@ function createItem(req, res) {
     item.save(item)
         .then(() => res.status(201).json({ message: 'Object recorded !' }))
         .catch((error) => res.status(400).json({ error }));
-}
+};
 
 /**
  * update an item
- * @param {Express.Request} req 
- * @param {Express.Response} res 
+ * @param {Express.Request} req
+ * @param {Express.Response} res
  */
-function updateItem(req, res) {
+exports.updateItem = (req, res) => {
     Item.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Object updated !' }))
         .catch((error) => res.status(400).json({ error }));
-}
+};
 
 /**
  * get one item by id
- * @param {Express.Request} req 
- * @param {Express.Response} res 
+ * @param {Express.Request} req
+ * @param {Express.Response} res
  */
-function getOneItemById(req, res) {
+exports.getOneItemById = (req, res) => {
     Item.findOne({ _id: req.params.id })
         .then((item) => res.status(200).json(item))
         .catch((error) => res.status(404).json({ error }));
-}
+};
 
 /**
  * get all items
- * @param {Express.Request} req 
- * @param {Express.Response} res 
+ * @param {Express.Request} req
+ * @param {Express.Response} res
  */
-function getAllItems() {
+exports.getAllItems = (req, res) => {
     Item.find()
         .then((items) => res.status(200).json(items))
         .catch((error) => res.status(401).json({ error }));
-}
+};
 
 /**
  * delete one item
- * @param {Express.Request} req 
- * @param {Express.Response} res 
+ * @param {Express.Request} req
+ * @param {Express.Response} res
  */
-function deleteItem() {
+exports.deleteItem = (req, res) => {
     Item.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Object deleted !' }))
         .catch((error) => res.status(400).json({ error }));
-}
-
-module.exports = { createItem, updateItem, getOneItemById, getAllItems, deleteItem }
+};
--- server/routes/item.route.js
+++ server/routes/item.route.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const router = express.Router();
-import { createItem, updateItem, getOneItemById, getAllItems, deleteItem } from '../controllers/item.controller'
+const itemCtrl = require('../controllers/item.controller');
 
-router.post('/', createItem);
-router.put('/:id', updateItem);
-router.get('/:id', getOneItemById);
-router.get('/', getAllItems);
-router.get('/:id', deleteItem)
-
-
-// router.get('/', (req, res) => {
-//     Item.find()
-//         .then((items) => res.status(200).json(items))
-//         .catch((error) => res.status(401).json({ error }));
-// });
-
-// router.delete('/:id', (req, res) => {
-//     Item.deleteOne({ _id: req.params.id })
-//         .then(() => res.status(200).json({ message: 'Object deleted !' }))
-//         .catch((error) => res.status(400).json({ error }));
-// });
+router.post('/', itemCtrl.createItem);
+router.put('/:id', itemCtrl.updateItem);
+router.get('/:id', itemCtrl.getOneItemById);
+router.get('/', itemCtrl.getAllItems);
+router.get('/:id', itemCtrl.deleteItem);
 
 module.exports = router;
