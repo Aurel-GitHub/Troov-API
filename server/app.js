2da0a12181f6a6f8b45947289715ef5fe6a02086
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-07-07 16:31:48 +0200

    add normalizePort and errorHandler to server

--- server/app.js
+++ server/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use((req, res) => {
+  res.status(201);
   res.json({
-    message: 'la requete a bien ete recue',
+    message: 'fdsf',
+
   });
 });
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,67 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-console */
 require('dotenv').config();
-const http = require('http');
 
+const http = require('http');
 const app = require('./app');
 
-app.set('port', process.env.DEFAULT_PORT || process.env.PORT);
+/**
+ * sends a valid port, whether provided as a number, a false value or a string.
+ *
+ * @param {string} val
+ * @returns {string, false, number}
+ */
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+const port = normalizePort(process.env.PORT_DEFAULT || process.env.PORT);
+app.set('port', port);
+
+/**
+ * searches for the various errors and handles them appropriately.
+ * It is then recorded in the server.
+ *
+ * @param {unknown} error
+ * @return {void}
+ */
+function errorHandler(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const address = server.address();
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges.`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use.`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
+  console.log(`Listening on ${bind}`);
+});
+
+server.listen(port);
