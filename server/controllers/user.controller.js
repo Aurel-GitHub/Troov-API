1142e4dc28582867721174a1d67f5ab34061113b
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-07-08 10:13:35 +0200

    add signup & login to user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,15 +1,55 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 /**
- *
- * @param {*} req
- * @param {*} res
+ * signup user
+ * @param {Express.Request} req
+ * @param {Express.Response} res
  */
-exports.signup = (req, res) => {};
+exports.signup = (req, res) => {
+    bcrypt
+        .hash(req.body.password, 10)
+        .then((hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash,
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'User created !' }))
+                .catch((error) => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
 
 /**
- *
- * @param {*} req
- * @param {*} res
+ * login user
+ * @param {Express.Request} req
+ * @param {Express.Response} res
  */
-exports.login = (req, res) => {};
+exports.login = (req, res) => {
+    User.findOne({
+        email: req.body.email,
+    })
+        .then((user) => {
+            if (user === null) {
+                res.status(401).json({ message: 'email password incorrect' });
+            } else {
+                bcrypt
+                    .compare(req.body.password, user.password)
+                    .then((valid) => {
+                        if (!valid) {
+                            res.status(401).json({
+                                message: 'email password incorrect',
+                            });
+                        } else {
+                            res.status(200).json({
+                                userId: user._id,
+                                token: 'TOKEN',
+                            });
+                        }
+                    })
+                    .catch((error) => res.status(500).json({ error }));
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
